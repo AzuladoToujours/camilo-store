a5363785cff25f677fca752683ac28394dc5b333
Camilo <camiloa.mejia@udea.edu.co>
2021-03-21 14:03:42 -0500

    Adding mercadolibre's font to header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import '../styles/components/Footer.styl';
 
 const Footer = () => (
   <div className="Footer">
-    <p className="Footer-title">Camilito Store</p>
+    <p className="Footer-title">Camilo Store</p>
     <ul className="Footer-social-icons">
       <li><a className="github" href="https://github.com/AzuladoToujours/camilo-store"><i className="fa fa-github" /></a></li>
       <li><a className="linkedin" href="https://www.linkedin.com/in/camilo-mej%C3%ADa-71096a201/"><i className="fa fa-linkedin" /></a></li>
     </ul>
-    <p className="Footer-copy">Empresariales 2021</p>
+    <p className="Footer-course">Empresariales 2021</p>
   </div>
 );
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = props => (
   <div className="Header">
     <h1 className="Header-title">
       <Link to="/">
-        Camilito Store
+        Camilo Store
       </Link>
     </h1>
     <div className="Header-checkout">
