c5c73ed39afe535ddeca47780636b06031ba58cb
Camilo <camiloa.mejia@udea.edu.co>
2021-03-21 10:46:15 -0500

    Fixing deploy to heroku v5

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => (
   <div className="Footer">
     <p className="Footer-title">Camilito Store</p>
     <ul className="Footer-social-icons">
-      <li><a className="github" href="#"><i className="fa fa-github" /></a></li>
+      <li><a className="github" href="https://github.com/AzuladoToujours/camilo-store"><i className="fa fa-github" /></a></li>
       <li><a className="linkedin" href="https://www.linkedin.com/in/camilo-mej%C3%ADa-71096a201/"><i className="fa fa-linkedin" /></a></li>
     </ul>
     <p className="Footer-copy">Empresariales 2021</p>
